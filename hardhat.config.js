283954df997af4e3bdabe356e6728d18eb984dc9
Ayodeji Omoloja <riley@Ayodejis-MacBook-Air.local>
2022-03-10 17:58:56 +0100

    updated hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,23 @@
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+
+require('dotenv').config
+//loads dotenv information
+require('@nomiclabs/hardhat-ethers')
+// helps with deploy script
+
+const { API_URL, PRIVATE_KEY } = process.env
+
 module.exports = {
   solidity: "0.7.3",
+  defaultNetwork: "rinkeby",
+  networks: {
+    hardhat: {},
+    rinkeby: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
+    },
+  }
+
 };
